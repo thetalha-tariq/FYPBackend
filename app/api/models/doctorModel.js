20b3eea05f43406c0c0c0f90376959d70f52c61e
Muhammad Ahmad <daniyalabbas001@gmail.com>
2024-07-06 15:06:36 +0500

    latest

---
+++ app/api/models/doctorModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        specialization: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        experience: {
+            type: Number,
+            required: true,
+        },
+        qualifications: {
+            type: [String],
+            required: true,
+        },
+        availability: {
+            type: [String],
+            required: true,
+        },
+        userRole: {
+            type: String,
+            trim: true,
+            default: "doctor",
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const doctorModel = mongoose.model("doctors", doctorSchema);
+
+module.exports = doctorModel;
